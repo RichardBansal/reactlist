46a1e6936e4115959ea822f102f97b9f3e12db88
Richard Bansal <richard.bansal@gmail.com>
2015-05-01 15:49:10 -0400

    successfully scrapped nomadlist data to cityData.json

--- js/nomadlist-scrapping.js
+++ js/nomadlist-scrapping.js
@@ -9,26 +9,42 @@ bluebird.promisifyAll(request); //now all request functions have an Async functi
 bluebird.promisifyAll(fs);
 
 //function (error, response, body) multiple promises are created in an array, you need to response or body
-var promise = request.getAsync('https://reddit.com')
-
-console.log(promise);
-
+console.log('making request');
+var promise = request.getAsync('https://nomadlist.com')
+// console.log(promise);
 promise.get(1).then(fulfilled);
 
 function fulfilled(htmlBody){
 	var $ = cheerio.load(htmlBody);
-	var result = "["
+	var result = "[";
 	// var arrOfTitles = 
 	// console.log(arrOfTitles);
-	console.log('testing');
-	$('.title.may-blank').each(function(i,title){
-		result += JSON.stringify(
-							{title:($(this).text())}
-						) + ",";
-		console.log(title);
+	
+	var cityData = $(".leaderbox > a > div") //.slice(0,10);
+	//'.title.may-blank'
+	// arr = arr.slice(0,2);
+	
+	// arr.forEach(function(city){
+	cityData = Array.prototype.slice.apply(cityData);
+	// console.log(arr[0].attribs["data-nomads-here"], Array.isArray(arr));
+	cityData.forEach(function(city){
+		// console.log(city.attribs);
+		result += JSON.stringify(city.attribs) + ","
 	});
-	result += "]"
-	fs.writeFileAsync('test.txt',result).then(doneWriting)
+	// });
+	// arr.forEach(function(city,index){
+		// if(index === 0){ console.log(city)}
+	// })
+	
+	// $(arr).each(function(index,div){
+		// result += JSON.stringify(
+							// {title:($(this).text())}
+						// ) + ",";
+		// console.log(title);
+	// });
+	result += "]";
+
+	fs.writeFileAsync('cityData.json',result).then(doneWriting)
 		function doneWriting(response){
 			console.log('Done Writing');
 	}
